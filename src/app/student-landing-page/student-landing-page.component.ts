15ddcc2afc3e45603eaf7edd6203f1b021cc54d5
AMAN291-as <sinhaaman2180@gmail.com>
2024-11-05 09:46:58 +0530

    aman part completed

---
+++ src/app/student-landing-page/student-landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-landing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-landing-page.component.html',
+  styleUrl: './student-landing-page.component.css'
+})
+export class StudentLandingPageComponent {
+
+}
